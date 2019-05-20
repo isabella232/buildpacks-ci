eef1ceaffb6094c57638f766a9efbef1c9d2869f
Daniel Thornton <dthornton@pivotal.io>
2019-05-20 11:19:34 -0400

    fix task adding github release notes to work without libcfbuildpack release
    [#164881372]
    
    Co-authored-by: Tilly Taylor <mataylor@pivotal.io>

--- tasks/finalize-cnb/run.rb
+++ tasks/finalize-cnb/run.rb
@@ -1,19 +1,25 @@
 #!/usr/bin/env ruby
-# encoding: utf-8
 
 require 'fileutils'
-puts 'yar'
+
 artifact_dir = File.join(Dir.pwd, 'release-artifacts')
 release_body_file = File.join(artifact_dir, 'body')
 buildpack_repo_dir = 'buildpack'
 
-puts 'yar2'
+
+# TODO: remove this cloning when we have a new libcfbuildpack release
+`git clone https://github.com/cloudfoundry/libcfbuildpack`
+startingDir = Dir.pwd
+Dir.chdir('libcfbuildpack/packager')
+`go build -o my_packager`
+`mv my_packager ../../buildpack`
+Dir.chdir(startingDir)
 
 Dir.chdir(buildpack_repo_dir) do
   go_mod_file = File.file?("go.mod")
-  puts 'yar3'
   if go_mod_file
-    `go install github.com/cloudfoundry/libcfbuildpack/packager`
-    File.write(release_body_file, `packager summary`, mode: 'a')
+      # should be replaced when we cut a new libcfbuildpack release with -summary flag
+      # `go install github.com/cloudfoundry/libcfbuildpack/packager`
+    File.write(release_body_file, `./my_packager -summary`, mode: 'a')
   end
 end
