12df78e7582ffc9567b7ca6002b5077916b27a9e
Chhavi Kankaria <ckankaria@pivotal.io>
2019-07-25 17:29:45 -0400

    [#163461758]
    Co-authored-by: Chhavi Kankaria <ckankaria@pivotal.io>

--- tasks/write-plugins-file/run.sh
+++ tasks/write-plugins-file/run.sh
@@ -4,22 +4,41 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-NEW_PACK_VERSION=$(cat pack/version)
-sed "s/PACK_VERSION=\".*\"/PACK_VERSION=\"$NEW_PACK_VERSION\"/g" -i cnb-scripts/install_tools.sh
-rsync -a cnb-scripts/ updated-cnb-scripts/
+version="$(cat version/version)"
 
-pushd updated-cnb-scripts
-  git add .
+calculate_checksum() {
+    osx="$1"
+    wget https://github.com/cloudfoundry/stack-auditor/releases/download/v"$version"/stack-auditor-"$version"-"$osx".tgz
+    local checksum="$(shasum -a 1 stack-auditor-"$version"-"$osx".tgz | cut -d " " -f 1)"
+    echo "$checksum"
+}
 
-  set +e
-    git diff --cached --exit-code
-    no_changes=$?
-  set -e
 
-  if [ $no_changes -ne 0 ]
-  then
-    git commit -m "Update pack to version $NEW_PACK_VERSION"
-  else
-    echo "pack is up to date"
-  fi
-popd
+
+
+
+
+cat <<EOF > metadata.yml
+- authors:
+  - contact: cf-buildpacks-eng@pivotal.io
+    name: Pivotal Buildpacks team
+  binaries:
+  - checksum: "$(calculate_checksum darwin)"
+    platform: osx
+    url: https://github.com/cloudfoundry/stack-auditor/releases/download/v"$version"/stack-auditor-"$version"-darwin.tgz
+  - checksum: "$(calculate_checksum windows)"
+    platform: win64
+    url: https://github.com/cloudfoundry/stack-auditor/releases/download/v"$version"/stack-auditor-"$version"-windows.zip
+  - checksum: "$(calculate_checksum linux)"
+    platform: linux64
+    url: https://github.com/cloudfoundry/stack-auditor/releases/download/v"$version"/stack-auditor-"$version"-linux.tgz
+  company: Pivotal
+  created: 2019-07-25T15:32:00Z
+  description: Provides commands for listing apps and their stacks, migrating apps
+    to a new stack, and deleting a stack
+  homepage: https://github.com/cloudfoundry/stack-auditor
+  name: stack-auditor
+  updated: 2019-07-18T14:24:60Z
+  version: $version
+
+EOF
